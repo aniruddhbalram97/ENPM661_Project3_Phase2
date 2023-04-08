05e60b2940118f4111aa09faa7d96a33f51e146c
Aniruddh Balram <abalram1@umd.edu>
2023-04-07 21:28:25 -0400

    Added function to check for valid input

--- a_star_visualization.py
+++ a_star_visualization.py
@@ -40,3 +40,19 @@ def enter_coordinates(obstacle_map):
     g = (x_g, obstacle_map.shape[0] - y_g - 1)
     
     return i, g, rpm
+
+
+# check for valid input coordinates
+def check_valid_entry(start, goal, obstacle_map, robot_radius, clearance):
+    net_clearance = robot_radius + clearance
+    net_clearance = int(round(net_clearance))
+    
+    if(obstacle_map[start[1]][start[0]][0]==255 or obstacle_map[goal[1]][goal[0]][0]==255 or 
+      (start[0] - net_clearance) < 0 or (start[0] + net_clearance) > 599 or (start[1] - net_clearance) < 0 or (start[1] + net_clearance) > 199
+       or sum(obstacle_map[goal[1]][goal[0]])==765 or sum(obstacle_map[start[1]][start[0]])==765
+      ):
+        print("The start point or end point is invalid. Either it is out of bounds or within obstacle space. Try Again!\n")
+        return False
+    else:
+        print("Great! The start and goal points are valid")
+        return True
